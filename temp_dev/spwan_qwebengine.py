819112c0570180b53d7332105b0ae52a06887c43
lrpirlet <lrpirlet@gmail.com>
2022-01-06 14:46:59 +0100

    Added one toolbar in the bottom of WebEngineView with 3 buttons: isbn, autor(s) and title

--- temp_dev/spwan_qwebengine.py
+++ temp_dev/spwan_qwebengine.py
@@ -10,62 +10,93 @@ import sys
 
 class MainWindow(QMainWindow):
 
-#    def __init__(self, log, url, que):
     def __init__(self, url, que):
         super(MainWindow,self).__init__()
-        
+
         self.que = que
-#        self.log = log
-#        print("In __init__,  log : ", type(log), log)
-#        print("In __init__,  que : ", type(que), que)        
 
         self.browser = QWebEngineView()
-        self.browser.setGeometry(300, 300, 1200, 800)
+#        self.browser.setGeometry(100, 100, 1200, 800)
+        self.browser.resize(1200,800)
         self.browser.setUrl(QUrl(url))
 
         self.setCentralWidget(self.browser)
 
-        navtb = QToolBar("Navigation")
-        navtb.setIconSize(QSize(20,20))
-        self.addToolBar(navtb)
+        get_tb = QToolBar("Get")
+        get_tb.setIconSize(QSize(60,30))
+        self.addToolBar(Qt.BottomToolBarArea, get_tb)
 
-        home_btn = QAction(QIcon('./blue/home.png'), "home", self)
-        home_btn.setStatusTip("On va à la une de noosfere")
+        ISBN_btn = QAction(QIcon('./blue_icon/ISBN.png'), "ISBN", self)
+        ISBN_btn.setStatusTip("Montre et copie le ISBN pour coller dans Mots-clefs à rechercher")
+        ISBN_btn.triggered.connect(self.navigate_home)
+        get_tb.addAction(ISBN_btn)
+
+        Auteurs_btn = QAction(QIcon('./blue_icon/Auteurs.png'), "Auteur(s)", self)
+        Auteurs_btn.setStatusTip("Montre et copie le(s) Auteur(s) pour coller dans Mots-clefs à rechercher")
+        Auteurs_btn.triggered.connect(self.browser.back)
+        get_tb.addAction(Auteurs_btn)
+
+        Titre_btn = QAction(QIcon('./blue_icon/Titre.png'), "Titre", self)
+        Titre_btn.setStatusTip("Montre le Titre")
+        Titre_btn.triggered.connect(self.browser.forward)
+        get_tb.addAction(Titre_btn)
+
+        self.getbar = QLineEdit()
+        self.getbar.setReadOnly(True)
+        self.getbar.setStatusTip("Aucune action, montre l'ISBN, le(s) Auteur(s) ou le Titre, protégé en écriture")
+                                # No action displays the ISBN the Author(s) or the Title, in write protect
+        get_tb.addWidget(self.getbar)
+
+        nav_tb = QToolBar("Navigation")
+        nav_tb.setIconSize(QSize(20,20))
+        self.addToolBar(nav_tb)
+
+        home_btn = QAction(QIcon('./blue_icon/home.png'), "Home", self)
+        home_btn.setStatusTip("On va à la une de noosfere")                         # We go to the front page of noosfere
         home_btn.triggered.connect(self.navigate_home)
-        navtb.addAction(home_btn)
+        nav_tb.addAction(home_btn)
 
-        back_btn = QAction(QIcon('./blue/back.png'), "Back", self)
-        back_btn.setStatusTip("On revient à la page précédente")
+        back_btn = QAction(QIcon('./blue_icon/back.png'), "Back", self)
+        back_btn.setStatusTip("On revient à la page précédente")                    # Back to the previous page
         back_btn.triggered.connect(self.browser.back)
-        navtb.addAction(back_btn)
+        nav_tb.addAction(back_btn)
 
-        next_btn = QAction(QIcon('./blue/forward.png'), "Forward", self)
-        next_btn.setStatusTip("On retourne à la page précédente")
+        next_btn = QAction(QIcon('./blue_icon/forward.png'), "Forward", self)
+        next_btn.setStatusTip("On retourne à la page suivante")                     # Back to the next page
         next_btn.triggered.connect(self.browser.forward)
-        navtb.addAction(next_btn)
+        nav_tb.addAction(next_btn)
 
-        reload_btn = QAction(QIcon('./blue/reload.png'), "Reload", self)
-        reload_btn.setStatusTip("On recharge la page")
+        reload_btn = QAction(QIcon('./blue_icon/reload.png'), "Reload", self)
+        reload_btn.setStatusTip("On recharge la page")                              # Reload the page
         reload_btn.triggered.connect(self.browser.reload)
-        navtb.addAction(reload_btn)
-        
-        stop_btn = QAction(QIcon('./blue/stop.png'), "stop", self)
-        stop_btn.setStatusTip("stop loading page")
+        nav_tb.addAction(reload_btn)
+
+        stop_btn = QAction(QIcon('./blue_icon/stop.png'), "Stop", self)
+        stop_btn.setStatusTip("On arrête de charger la page")                       # Stop loading the page
         stop_btn.triggered.connect(self.browser.stop)
-        navtb.addAction(stop_btn)
+        nav_tb.addAction(stop_btn)
 
         self.urlbar = QLineEdit()
         self.urlbar.returnPressed.connect(self.navigate_to_url)
-        navtb.addWidget(self.urlbar)
+        self.urlbar.setStatusTip("Tu peut même introduire une adresse, hors noosfere, mais A TES RISQUES ET PERILS... noosfere est sûr ( https:// ), la toile par contre...")
+                                # You can even enter an address, outside of noosfere, but AT YOUR OWN RISK... noosfere is safe: ( https:// ), the web on the other side...
+        nav_tb.addWidget(self.urlbar)
 
         self.browser.urlChanged.connect(self.update_urlbar)
         self.browser.loadFinished.connect(self.update_title)
 
-        exit_btn = QAction(QIcon('./blue/exit.png'), "Select and exit", self)
-        exit_btn.setStatusTip("on sélectionne cet URL pour extraction de nsfr_id et on retourne à calibre")
+        abort_btn = QAction(QIcon('./blue_icon/abort.png'), "Abort", self)
+        abort_btn.setStatusTip("On arrête tout, on oublie tout et on ne change rien")
+                              # Stop everything, forget everything and change nothing
+        abort_btn.triggered.connect(self.close)
+        nav_tb.addAction(abort_btn)
+
+        exit_btn = QAction(QIcon('./blue_icon/exit.png'), "Select and exit", self)
+        exit_btn.setStatusTip("On sélectionne cet URL pour extraction de nsfr_id, on continue")
+                             # select this URL for extraction of nsfr_id, continue
         exit_btn.triggered.connect(self.select_and_exit)
-        navtb.addAction(exit_btn)
-        
+        nav_tb.addAction(exit_btn)
+
         self.show()
 
         self.setStatusBar(QStatusBar(self))
@@ -73,7 +104,7 @@ class MainWindow(QMainWindow):
     def navigate_home(self):
         self.browser.setUrl( QUrl("https://www.noosfere.org/") )
 
-    def navigate_to_url(self): # Does not receive the Url
+    def navigate_to_url(self):                    # Does not receive the Url
         q = QUrl( self.urlbar.text() )
         self.browser.setUrl(q)
         print("In navigate_to_url  URL : ", self.urlbar.text())
@@ -86,14 +117,21 @@ class MainWindow(QMainWindow):
         title = self.browser.page().title()
         self.setWindowTitle(title)
 
-    def select_and_exit(self):              #sent q to the queue wait till consumed then exit
+    def select_and_exit(self):                    #sent q to the queue wait till consumed then exit
         self.que.put(self.urlbar.text())
         sys.exit(0)
 
-    def closeEvent(self, event):    # hit window exit "X" button
-        self.que.put("window was closed")
-        super().closeEvent(event)
-        
+    def closeEvent(self, event):                  # hit window exit "X" button
+        qDebug('MainWindow.closeEvent()')
+        reply = QMessageBox.question(self, 'Vraiment', "Quitter et ne rien changer", QMessageBox.No | QMessageBox.Yes, QMessageBox.Yes)
+        qDebug('MainWindow.closeEvent()'+str(reply))
+        if reply == QMessageBox.Yes:
+            event.accept()
+            self.que.put("")
+            super().closeEvent(event)
+        else:
+            event.ignore()
+
 def spawned_main(que, url):
 #    logfile = open("spanlog.txt", "a")
 #    txt=time.strftime("%D %H:%M:%S", time.localtime())+' logfile is open now'
@@ -104,12 +142,13 @@ def spawned_main(que, url):
 #    window = MainWindow(logfile, url, que)
     window = MainWindow(url, que)
     app.exec_()
-    
+
 if __name__ == '__main__':
     url="https://www.noosfere.org/livres/noosearch.asp"
     que = Queue()
     prc = Process(target=spawned_main, args=(que, url))
     prc.start()
-    print("In main, la dernier url est: ", que.get())
+    response = que.get()
+    print("In main, la dernier url est: ", response, "len",  len(response), "type", type(response))
     prc.join()
-    
+

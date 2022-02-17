3e185dbb5376937a616fbe0e82ba1b8ded64b989
lrpirlet <lrpirlet@gmail.com>
2022-02-17 11:06:16 +0100

    modified qwebengine to hide search bar, and some more goodies

--- temp_dev/qwebengine.py
+++ temp_dev/qwebengine.py
@@ -1,7 +1,7 @@
-from PyQt5.QtCore import pyqtSlot, qDebug, QUrl, QSize
+from PyQt5.QtCore import pyqtSlot, qDebug, QUrl, QSize, Qt
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QToolBar, QAction, QLineEdit,
                                 QStatusBar, QMessageBox, qApp, QWidget, QVBoxLayout,
-                                QHBoxLayout, QPushButton, QShortcut)
+                                QHBoxLayout, QPushButton, QShortcut, QDockWidget)
 from PyQt5.QtGui import QIcon, QKeySequence
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 from json import dumps
@@ -21,14 +21,37 @@ class MainWindow(QMainWindow):
 
         self.cb = QApplication.clipboard()
 
-  # browser
+        self.set_browser()
+        self.set_isbn_box()
+        self.set_auteurs_box()
+        self.set_titre_box()
+        self.join_all_boxes()
+        self.set_search_box()
+        self.set_search_dock()
+        self.set_nav_and_status_bar()
+
+      # make all that visible
+
+        self.show()
+
+      # signals
+
+        self.browser.urlChanged.connect(self.update_urlbar)
+        self.browser.urlChanged.connect(self.srch_dsp.clear)
+        self.browser.urlChanged.connect(self.search_dock.hide)
+        self.browser.loadStarted.connect(self.loading_title)
+        self.browser.loadProgress.connect(self.reloading_title)
+        self.browser.loadFinished.connect(self.update_title)
+        self.search_dock.visibilityChanged.connect(self.srch_dsp.clear)
+
+    def set_browser(self):                      # browser
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl("http://www.google.com"))
 
-  # info boxes isbn
+    def set_isbn_box(self):                     # info boxes isbn
         self.isbn_btn = QPushButton(" ISBN ", self)
-        self.isbn_btn.setStatusTip("Copie le ISBN dans le presse-papier pour coller dans Mots-clefs à rechercher")
-                                   # Show authors, copy in clipboard to paste in search field of noosfere
+        self.isbn_btn.setStatusTip('Page initiale: "Mots-clefs à rechercher" = ISBN, coche la case "Livre"... Ailleur: boite de recherche = ISBN')
+                                   # Home page: "Mots-clefs à rechercher" = ISBN, set checkbox "Livre"... anywhere else: search box = ISBN
         self.isbn_btn.clicked.connect(self.set_isbn_info)
 
         self.isbn_dsp = QLineEdit()
@@ -43,10 +66,10 @@ class MainWindow(QMainWindow):
         self.isbn_lt.addWidget(self.isbn_btn)
         self.isbn_lt.addWidget(self.isbn_dsp)
 
-  # info boxes auteurs
+    def set_auteurs_box(self):                  # info boxes auteurs
         self.auteurs_btn = QPushButton("Auteur(s)", self)
-        self.auteurs_btn.setStatusTip("Copie le(s) Auteur(s) dans le presse-papier pour coller dans Mots-clefs à rechercher")
-                                      # Show authors, copy in clipboard to paste in search field of noosfere
+        self.auteurs_btn.setStatusTip('Page initiale: "Mots-clefs à rechercher" = Auteur(s), coche la case "Auteurs"... Ailleur: boite de recherche = Auteurs')
+                                      # Home page: "Mots-clefs à rechercher" = Auteur(s), set checkbox "Auteurs"... anywhere else: search box = Auteurs
         self.auteurs_btn.clicked.connect(self.set_auteurs_info)
 
         self.auteurs_dsp = QLineEdit()
@@ -61,9 +84,10 @@ class MainWindow(QMainWindow):
         self.auteurs_lt.addWidget(self.auteurs_btn)
         self.auteurs_lt.addWidget(self.auteurs_dsp)
 
-  # info boxes titre
+    def set_titre_box(self):                    # info boxes titre
         self.titre_btn = QPushButton("Titre", self)
-        self.titre_btn.setStatusTip("Copie le Titre dans le presse-papier pour coller dans Mots-clefs à rechercher")                                   # show title
+        self.titre_btn.setStatusTip('Page initiale: "Mots-clefs à rechercher" = Titre, coche la case "Livres"... Ailleur: boite de recherche = Titre')
+                                    # Home page: "Mots-clefs à rechercher" = Titre, set checkbox "Livres"... anywhere else: search box = Titre
         self.titre_btn.clicked.connect(self.set_titre_info)
 
         self.titre_dsp = QLineEdit()
@@ -78,8 +102,20 @@ class MainWindow(QMainWindow):
         self.titre_lt.addWidget(self.titre_btn)
         self.titre_lt.addWidget(self.titre_dsp)
 
-  # search box and buttons
+    def join_all_boxes(self):                   # put all that together, center, size and make it central widget
+        layout = QVBoxLayout()
+        layout.addWidget(self.browser)
+        layout.addLayout(self.isbn_lt)
+        layout.addLayout(self.auteurs_lt)
+        layout.addLayout(self.titre_lt)
 
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+        self.resize(1200,1000)
+
+    def set_search_box(self):                   # search box and buttons
         self.next_btn = QPushButton('Suivant', self)
         self.next_btn.clicked.connect(self.update_searching)
         self.next_btn.setStatusTip("Cherche le suivant")
@@ -88,45 +124,41 @@ class MainWindow(QMainWindow):
         self.prev_btn = QPushButton('Précédent', self)
         self.prev_btn.clicked.connect(self.find_backward)
         self.prev_btn.setStatusTip("Cherche le précédant")
-        if isinstance(self.next_btn, QPushButton): self.prev_btn.clicked.connect(self.setFocus)
+        if isinstance(self.prev_btn, QPushButton): self.prev_btn.clicked.connect(self.setFocus)
 
         self.srch_dsp = QLineEdit()
         self.srch_dsp.setStatusTip("Contient le texte a rechercher. !!! Même le charactère espace compte !!!")
-        if isinstance(self.srch_dsp, QPushButton): self.srch_dsp.clicked.connect(self.setFocus)
+
         self.setFocusProxy(self.srch_dsp)
         self.srch_dsp.textChanged.connect(self.update_searching)
         self.srch_dsp.returnPressed.connect(self.update_searching)
-##        self.closed.connect(self.srch_dsp.clear)
 
         self.done_btn = QPushButton("Efface")
         self.done_btn.setStatusTip("Efface le contenu de la boite")
         self.done_btn.clicked.connect(self.srch_dsp.clear)
         if isinstance(self.done_btn, QPushButton): self.done_btn.clicked.connect(self.setFocus)
 
-        self.srch_lt = QHBoxLayout(self)
-        self.srch_lt.addWidget(self.next_btn)
-        self.srch_lt.addWidget(self.prev_btn)
-        self.srch_lt.addWidget(self.srch_dsp)
-        self.srch_lt.addWidget(self.done_btn)
-
-        QShortcut(QKeySequence.FindNext, self, activated=self.next_btn.animateClick)        # simule un click de souris de .1 secondes
+        QShortcut(QKeySequence.FindNext, self, activated=self.next_btn.animateClick)
         QShortcut(QKeySequence.FindPrevious, self, activated=self.prev_btn.animateClick)
 
-  # put all that together center and size it
-        layout = QVBoxLayout()
-        layout.addWidget(self.browser)
-        layout.addLayout(self.isbn_lt)
-        layout.addLayout(self.auteurs_lt)
-        layout.addLayout(self.titre_lt)
-        layout.addLayout(self.srch_lt)
+    def set_search_dock(self):                  # build a dockable windows
+        self.search_dock = QDockWidget("Fenêtre de recherche")
+        self.search_dock.setAllowedAreas(Qt.BottomDockWidgetArea)
+        self.search_dock_content = QWidget()
 
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
+        srch_lt = QHBoxLayout()
+        srch_lt.addWidget(self.next_btn)
+        srch_lt.addWidget(self.prev_btn)
+        srch_lt.addWidget(self.srch_dsp)
+        srch_lt.addWidget(self.done_btn)
 
-        self.resize(1200,1000)
 
-  # set navigation toolbar
+        self.search_dock_content.setLayout(srch_lt)
+        self.search_dock.setWidget(self.search_dock_content)
+        self.addDockWidget(Qt.BottomDockWidgetArea, self.search_dock)
+        self.search_dock.hide()
+
+    def set_nav_and_status_bar(self) :          # set navigation toolbar and status bar
         nav_tb = QToolBar("Navigation")
         nav_tb.setIconSize(QSize(20,20))
         self.addToolBar(nav_tb)
@@ -156,6 +188,14 @@ class MainWindow(QMainWindow):
         stop_btn.triggered.connect(self.browser.stop)
         nav_tb.addAction(stop_btn)
 
+        nav_tb.addSeparator()
+
+        srch_btn = QAction(QIcon('./blue_icon/search.png'), "Find", self)
+        srch_btn.setShortcut(QKeySequence.Find)
+        srch_btn.setStatusTip("Z'avez pas vu Mirza? Oh la la la la la. Où est donc passé ce chien. Je le cherche partout...  (Merci Nino Ferrer)")                       # Stop loading the page
+        srch_btn.triggered.connect(self.search_dock.show)
+        nav_tb.addAction(srch_btn)
+
         self.urlbox = QLineEdit()
         self.urlbox.returnPressed.connect(self.navigate_to_url)
         self.urlbox.setStatusTip("Tu peut même introduire une adresse, hors noosfere, mais A TES RISQUES ET PERILS... noosfere est sûr (https://), la toile par contre...")
@@ -174,18 +214,8 @@ class MainWindow(QMainWindow):
         exit_btn.triggered.connect(self.select_and_exit)
         nav_tb.addAction(exit_btn)
 
-        self.browser.urlChanged.connect(self.update_urlbar)
-        self.browser.urlChanged.connect(self.srch_dsp.clear)
-        self.browser.loadStarted.connect(self.loading_title)
-        self.browser.loadProgress.connect(self.reloading_title)
-        self.browser.loadFinished.connect(self.update_title)
-
-  # set status bar
         self.setStatusBar(QStatusBar(self))
 
-  # make all that visible
-
-        self.show()
 
  # search action
     @pyqtSlot()
@@ -203,34 +233,44 @@ class MainWindow(QMainWindow):
 
   # info boxes actions
     def set_noosearch_page(self, html_str):
-        if self.urlbox.text() == "https://www.noosfere.org/livres/noosearch.asp":
-            self.html = html_str
-            if self.iam == "isbn": val = self.isbn
-            elif self.iam == "auteurs": val = self.auteurs
-            else: val = self.titre 
-            self.browser.page().runJavaScript("document.getElementsByName('Mots')[1].value =" + dumps(val))
-            if self.iam == "auteurs":
-                self.browser.page().runJavaScript("document.getElementsByName('auteurs')[0].checked = true")
-                self.browser.page().runJavaScript("document.getElementsByName('livres')[0].checked = false")
-            else:
-                self.browser.page().runJavaScript("document.getElementsByName('livres')[0].checked = true")
-                self.browser.page().runJavaScript("document.getElementsByName('auteurs')[0].checked = false")
+        self.html = html_str
+        if self.iam == "isbn": val = self.isbn
+        elif self.iam == "auteurs": val = self.auteurs
+        else: val = self.titre
+        self.browser.page().runJavaScript("document.getElementsByName('Mots')[1].value =" + dumps(val))
+        if self.iam == "auteurs":
+            self.browser.page().runJavaScript("document.getElementsByName('auteurs')[0].checked = true")
+            self.browser.page().runJavaScript("document.getElementsByName('livres')[0].checked = false")
+        else:
+            self.browser.page().runJavaScript("document.getElementsByName('livres')[0].checked = true")
+            self.browser.page().runJavaScript("document.getElementsByName('auteurs')[0].checked = false")
 
     @pyqtSlot()
     def set_isbn_info(self):
-        self.isbn_dsp.setText(self.isbn)
-        self.iam = "isbn"
-        self.browser.page().toHtml(self.set_noosearch_page)
+        if self.urlbox.text() == "https://www.noosfere.org/livres/noosearch.asp":
+            self.iam = "isbn"
+            self.browser.page().toHtml(self.set_noosearch_page)
+        else:
+            self.search_dock.show()
+            self.srch_dsp.setText(isbn)
 
     @pyqtSlot()
     def set_auteurs_info(self):
-        self.iam = "auteurs"
-        self.browser.page().toHtml(self.set_noosearch_page)
+        if self.urlbox.text() == "https://www.noosfere.org/livres/noosearch.asp":
+            self.iam = "auteurs"
+            self.browser.page().toHtml(self.set_noosearch_page)
+        else:
+            self.search_dock.show()
+            self.srch_dsp.setText(auteurs)
 
     @pyqtSlot()
     def set_titre_info(self):
-        self.iam = titre
-        self.browser.page().toHtml(self.set_noosearch_page)
+        if self.urlbox.text() == "https://www.noosfere.org/livres/noosearch.asp":
+            self.iam = "titre"
+            self.browser.page().toHtml(self.set_noosearch_page)
+        else:
+            self.search_dock.show()
+            self.srch_dsp.setText(titre)
 
   # Navigation actions
     def initial_url(self, url="http://www.google.com"):
